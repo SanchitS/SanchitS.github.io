459f711e4cda4f4acd7e69e4c637f853bd527d3f
sanchits <sanchitsharma1209@gmail.com>
2016-03-16 17:02:27 +0530

    modal

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,4 +4,14 @@
          $(".nav li").removeClass("active");
          $(this).addClass("active");
      });
+
+     $(function () {
+         $('body').on('click', '.page-scroll a', function (event) {
+             var $anchor = $(this);
+             $('html, body').stop().animate({
+                 scrollTop: $($anchor.attr('href')).offset().top
+             }, 1500, 'easeInOutExpo');
+             event.preventDefault();
+         });
+     });
  });
