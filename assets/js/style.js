f8035fa79a0277312f4b857254209f6229e35fe3
Sanchit Sharma <sanchit.s@hotmail.com>
2017-12-03 16:36:28 +0530

    Coming Soon
    New portfolio commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,17 +1,32 @@
- $(document).ready(function () {
+function makeTimer() {
 
-//     $(".nav li").on("click", function () {
-//         $(".nav li").removeClass("active");
-//         $(this).addClass("active");
-//     });
-//
-//     $(function () {
-//         $('body').on('click', '.page-scroll a', function (event) {
-//             var $anchor = $(this);
-//             $('html, body').stop().animate({
-//                 scrollTop: $($anchor.attr('href')).offset().top
-//             }, 1500, 'easeInOutExpo');
-//             event.preventDefault();
-//         });
-//     });
- });
+			var endTime = new Date("05 March 2018 7:00:00 GMT+05:30");			
+			endTime = (Date.parse(endTime) / 1000);
+
+			var now = new Date();
+			now = (Date.parse(now) / 1000);
+
+			var timeLeft = endTime - now;
+
+			var days = Math.floor(timeLeft / 86400); 
+			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+  
+			if (hours < "10") { hours = "0" + hours; }
+			if (minutes < "10") { minutes = "0" + minutes; }
+			if (seconds < "10") { seconds = "0" + seconds; }
+
+			$("#days").html(days + "<span>Days</span>");
+			$("#hours").html(hours + "<span>Hours</span>");
+			$("#minutes").html(minutes + "<span>Minutes</span>");
+			$("#seconds").html(seconds + "<span>Seconds</span>");		
+
+	}
+
+	setInterval(function() { makeTimer(); }, 1000);
+
+//random images on the right side
+    var images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg'];
+    
+    $('<img class="fade-in" src="assets/img/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner-load');
